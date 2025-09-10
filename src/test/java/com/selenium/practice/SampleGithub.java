5eecb40b9a2699b86e362ff416737568d89b5254
SaikiranSikhakolli <saikiran.sikhakolli@gmail.com>
2025-09-10 23:20:08 +0530

    This is my second commit

---
+++ src/test/java/com/selenium/practice/SampleGithub.java
@@ -0,0 +1,11 @@
+package com.selenium.practice;
+
+public class SampleGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("My first github pratice");
+	}
+
+}
